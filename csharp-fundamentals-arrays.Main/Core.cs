7b3433c3a65b6092b1376b664e0ef042d42236a3
Ntina B <n_bekiari@outlook.com>
2023-08-31 16:17:39 +0300

    ok .

--- csharp-fundamentals-arrays.Main/Core.cs
+++ csharp-fundamentals-arrays.Main/Core.cs
@@ -9,13 +9,13 @@ namespace csharp_fundamentals_arrays.Main
 {
     public class Core
     {
-        // The block of code below is a method definition. So far, you've been using methods created by other people
-        // such as .length() and .charAt(n)
-        // From this exercise on, you'll be writing code inside methods very often! Methods are a container for a specific
-        // piece of logic that the class can run. For now, just implement your solutions between the comments that look like:
+        // The block of code below is a method definition. So far, you've been using methods created by other people such as .length() and .charAt(n)
+        // From this exercise on, you'll be writing code inside methods very often! Methods are a container for a specific piece of logic that the class can run. For now, just implement your solutions between the comments that look like:
+
         // WRITE YOUR CODE BETWEEN THIS LINE...
         // ... AND THIS LINE
         // See the example method below
+
         public void example()
         {
             //TODO: 1. Print the word "Hello"
@@ -26,75 +26,68 @@ namespace csharp_fundamentals_arrays.Main
 
         public int[] one()
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+
             int[] numbers = { 42, 13, 17, 91 };
 
             // 1. Values contained in an array are each stored at a unique numeric index, starting from 0 ascending in order.
             // E.g. The first value is at index 0, the second at index 1, the third at index 3.
             // Using an index, change the number 17 in the numbers array to 68
-            // WRITE YOUR CODE BETWEEN THIS LINE...
 
-
-
-            // ... AND THIS LINE
+            numbers[2] = 68;
 
             return numbers;
         }
 
         public String two()
         {
-            throw new NotImplementedException();
-            String[] teachers = { "Nathan", "Ed", "Dave", "Carlo", "Lewis", "Jules", "John", "Chris", "Nigel" };
+            // throw new NotImplementedException();
 
-            //TODO: 2. Using an array index, change the value of the teacher variable below to be the fourth
-            // teacher contained in the teachers array
-            // WRITE YOUR CODE BETWEEN THIS LINE...
+            string[] teachers = { "Nathan", "Ed", "Dave", "Carlo", "Lewis", "Jules", "John", "Chris", "Nigel" };
 
-            String teacher = "";
+            //TODO: 2. Using an array index, change the value of the teacher variable below to be the fourth teacher contained in the teachers array
 
-            // ... AND THIS LINE
+            string teacher = teachers[3];
 
             return teacher;
         }
 
         public String[] three()
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+
             //TODO: 3. Create a string array named cars that contains three names of car manufacturers: Audi, BMW and Dodge
-            // WRITE YOUR CODE BETWEEN THIS LINE...
 
+            string[] cars = { "Audi", "BMW", "Dodge" };
 
-
-            // ... AND THIS LINE
-
-            //return cars;
+            return cars;
         }
 
         public int four()
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+
             int[] numbers = { 42, 13, 17, 91 };
 
             // TODO 4. Using array indices, set the value of the result variable below to the sum of every number in the numbers array
-            // WRITE YOUR CODE BETWEEN THIS LINE...
 
             int result = 0;
 
-            // ... AND THIS LINE
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                result += numbers[i];
+            }
 
             return result;
         }
 
         public float[] five()
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+
             //TODO: 5. Create an array called floats that contains 3 floating point numbers: 9.62, 23.17 and 3.14
-            // WRITE YOUR CODE BETWEEN THIS LINE...
 
-            float[] floats;
-
-
-            // ... AND THIS LINE
+            float[] floats = { 9.62f, 23.17f, 3.14f };
 
             return floats;
         }
