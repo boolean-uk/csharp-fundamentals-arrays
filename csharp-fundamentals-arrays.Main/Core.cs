77978260402be9e5d3328202126a8affe222101d
Pia Loana Mullenbach <pia.loana.mullenbach@dk.experis.com>
2024-01-09 15:44:36 +0100

    nicolai

--- csharp-fundamentals-arrays.Main/Core.cs
+++ csharp-fundamentals-arrays.Main/Core.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
-
+//nicolai
 namespace csharp_fundamentals_arrays.Main
 {
     public class Core
